bb89d8967e822f6590023f09126d898197576720
Ahmed Mohammed <hmkalkali@gmail.com>
2025-02-21 18:12:26 +0100

    H

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -73,7 +73,7 @@ function Projects() {
               isBlog={false}
               title="Personal Expense Tracker"
               description="Developed personal expense tracker system that aims to help users manage their finances by tracking income and expenses."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              ghLink="https://github.com/AhmAlkalii/PersonalExpenseTracker"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
